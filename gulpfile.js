cdab8b09736eb4f1f220402fc79c899b8ceba6ab
Orest Bryk <Orest Bryk>
2016-06-07 17:44:25 +0300

    server changes, gulp removed

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-    argv = require('yargs').argv,
-    server = require('./server');
-
-
-gulp.task('http-prod-server', function () {
-    var port = argv.port || 80;
-    server.listen(port);
-});
--- server.js
+++ server.js
@@ -8,10 +8,5 @@ app.get('/*', function (req, res) {
     return res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-module.exports = {
-    listen: function (port) {
-        var server = app.listen(port, function () {
-            console.log('Words app listening at http://%s:%s', server.address().address, server.address().port);
-        });
-    }
-};
+
+app.listen(80);
